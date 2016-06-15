593bb65418c1af4c0097b7693d2aba3175c9032f
FWSeasonChange <fuwei_cn_jx@foxmail.com>
2016-06-15 08:58:53 +0800

    Initial Commit

---
+++ UITextView提示文字placeholder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITextView提示文字placeholder
+//
+//  Created by 付玮 on 16/6/15.
+//  Copyright © 2016年 付玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITextView提示文字placeholder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITextView提示文字placeholder
+//
+//  Created by 付玮 on 16/6/15.
+//  Copyright © 2016年 付玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
